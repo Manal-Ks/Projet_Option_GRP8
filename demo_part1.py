e2543713ea612310ff2a7f1e2ee5160d0512ab24
Manal-Ks <manal.kermoss@centrale-casablanca.ma>
2026-02-14 19:41:03 +0100

    Fix: update demo script + add gitignore

--- demo_part1.py
+++ demo_part1.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
 import pandas as pd
 
 from schema import validate_and_coerce, CANDIDATE_SCHEMA, JOB_SCHEMA
